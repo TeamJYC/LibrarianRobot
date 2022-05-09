2967e75629fed30c22f1b21f20ad55cc014eb1a0
Jae-Won-Choi <bullgokman@gmail.com>
2022-05-09 16:43:36 +0900

    Update camera.py

--- sensor/camera.py
+++ sensor/camera.py
@@ -12,15 +12,19 @@ def get_frame():
 
 def get_frame_every_nseconds(frame):
     img_source = frame
-    cv2.imshow('img_source', img_source)
     timer = Timer(DELAY_N_SECONDS, get_frame_every_nseconds(frame))
+    cv2.imshow('a', img_source)
     timer.start()
     return img_source
-
+    
 def camera_release():
     capture.release()
 
 if __name__ == "__main__":
+    flag = 0
     frame = get_frame()
-    img_source = get_frame_every_nseconds(frame)
+    if flag == 0:
+        img_source = get_frame_every_nseconds(frame)
+        flag = 1
     cv2.destroyAllWindows()
+    
