a093f7da05ba48404bafe57abab7bd9070d9f43f
Jae-Won-Choi <bullgokman@gmail.com>
2022-05-12 01:40:23 +0900

    0511-choi
    0511에 한 작업

--- camera.py
+++ camera.py
@@ -1,30 +1,19 @@
 import cv2
-from threading import Timer
+import time
 
 DELAY_N_SECONDS = 3
 
 capture = cv2.VideoCapture(0)
 
-def get_frame():
+def get_frame_every_nseconds():
     ret, frame = capture.read()
-    print("Camera on ...")
+    time.sleep(DELAY_N_SECONDS)
     return frame
 
-def get_frame_every_nseconds(frame):
-    img_source = frame
-    timer = Timer(DELAY_N_SECONDS, get_frame_every_nseconds(frame))
-    cv2.imshow('a', img_source)
-    timer.start()
-    return img_source
-    
 def camera_release():
     capture.release()
 
 if __name__ == "__main__":
-    flag = 0
-    frame = get_frame()
-    if flag == 0:
-        img_source = get_frame_every_nseconds(frame)
-        flag = 1
+    get_frame_every_nseconds()
     cv2.destroyAllWindows()
-    
+    camera_release()
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import cv2
 import camera
+import imgprocessing
 
-
-print("Starting Librarian ...")
-
-frame = camera.get_frame()
-
-img_source = camera.get_frame_every_nseconds(frame)
+while True:
+    img_source = camera.get_frame_every_nseconds()
+    
+    img_gray = cv2.cvtColor(img_source, cv2.COLOR_BGR2GRAY)
+    
+    img_watershed = imgprocessing.watershed(img_gray)
+    
+    img_warp = imgprocessing.perspectivetransformation(img_watershed)
+    
+    img_clache = imgprocessing.clache(img_warp)
+    
+    
+    
+    #전처리 & OCR
+    
+camera.camera_release()
