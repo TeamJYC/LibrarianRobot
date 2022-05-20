79799943b345a006be711a50911e6666af91700d
Jae-Won-Choi <bullgokman@gmail.com>
2022-05-20 19:35:23 +0900

    ver.0520 update
    textprocessing 모듈 신설, isbn, 청구기호 구분 기능 만듦

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import camera
-import imgprocessing
+import imgprocessing as ip
+import textprocessing as tp
 import ocr
 
 name = ["총류", "철학", "종교", "사회과학", "순수과학", "기술과학", "예슐", "언어", "문학", "역사"]
@@ -9,11 +10,11 @@ while True:
     img_source = camera.get_frame_every_nseconds()
     img_gray = cv2.cvtColor(img_source, cv2.COLOR_BGR2GRAY)
     
-    img_watershed = imgprocessing.watershed(img_gray, img_source)
+    img_watershed = ip.watershed(img_gray, img_source)
     
-    img_warp = imgprocessing.perspective_transformation(img_watershed, img_source)
+    img_warp = ip.perspective_transformation(img_watershed, img_source)
     
-    img_clache = imgprocessing.clache(img_warp)
+    img_clache = ip.clache(img_warp)
     
     img_gray2 = cv2.cvtColor(img_clache, cv2.COLOR_BGR2GRAY)
     ret, img_thresh = cv2.threshold(img_gray2, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
@@ -22,11 +23,20 @@ while True:
     cv2.imshow('img_source',img_source)
     
     text = ocr.tesseract(img_thresh)
-    destination = ocr.get_classnumber(text)
+    callnum = tp.get_callnum(text)
+    isbn = tp.get_isbn(text)
     
-    if (destination != None):
-        print(name[destination] + "분류입니다.")
+    kdc = None
+    
+    if callnum and not isbn :
+        kdc = tp.callnum_to_kdc(callnum)
+    elif isbn:
+        kdc = tp.isbn_to_kdc(isbn)
+        
+    if (kdc != None):
+        print(name[kdc] + "분류입니다.")
         break
+    
     key = cv2.waitKey(33)
     if key == 27: # Esc
         break
