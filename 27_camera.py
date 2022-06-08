fd4ce4fcf4e316a64a19d39e40e5d1cfbccd38bc
YoonSeMin <87269604+YoonSeMin@users.noreply.github.com>
2022-06-08 15:40:11 +0900

    python 2.7 수정

---
+++ 27_camera.py
@@ -0,0 +1,24 @@
+import cv2
+import time
+
+DELAY_N_SECONDS = 1
+
+capture = cv2.VideoCapture(0)
+
+def get_frame_every_nseconds():
+    ret, frame = capture.read()
+    time.sleep(DELAY_N_SECONDS)
+    return frame
+
+def camera_release():
+    capture.release()
+
+if __name__ == "__main__":
+    while True:
+        frame = get_frame_every_nseconds()
+        cv2.imshow('img', frame)
+        key = cv2.waitKey(33)
+        if key == 27: # Esc
+            break
+    cv2.destroyAllWindows()
+    camera_release()
